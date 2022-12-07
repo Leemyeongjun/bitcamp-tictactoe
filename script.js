cef3ca126d913849c0c8f1c117da92642675648f
Leemyeongjun <2myeongjun@naver.com>
2022-12-07 12:13:20 +0900

    test

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const boxs = document.querySelectorAll(".box");
 const statusText = document.querySelector("#statusText");
 const restartBtn = document.querySelector("#restartBtn");
- win_table = [
+const win_table = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
